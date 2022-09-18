6d1a0a89983ac2ebde830132311388497a1a1312
Notis Mastrandrikos <pmastrandrikos@gmail.com>
2022-09-18 15:22:45 +0300

    fix test paths

--- test/sampleProject/tests/CactusTest.php
+++ test/sampleProject/tests/CactusTest.php
@@ -8,8 +8,8 @@ final class CactusTest extends TestCase
 {
     public function testCheckCompiledFiles(): void
     {
-        $phpFileContents = file_get_contents(__DIR__.DIRECTORY_SEPARATOR."toBeCompiled".DIRECTORY_SEPARATOR."shouldBeCompiled.php");
-        $txtFileContents = file_get_contents(__DIR__.DIRECTORY_SEPARATOR."toBeCompiled".DIRECTORY_SEPARATOR."shouldNotBeCompiled.txt");
+        $phpFileContents = file_get_contents(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."toBeCompiled".DIRECTORY_SEPARATOR."shouldBeCompiled.php");
+        $txtFileContents = file_get_contents(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."toBeCompiled".DIRECTORY_SEPARATOR."shouldNotBeCompiled.txt");
 
         $this->assertTrue(str_contains($phpFileContents, "Cactus"));
         $this->assertTrue(str_contains($txtFileContents, "txt file"));
